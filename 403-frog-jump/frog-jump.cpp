9caa4f2668878e828708b6532ddfb8434edfe4ae
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-13 21:21:54 +0530

    Time: 890 ms (5.02%) | Memory: 214.7 MB (28.89%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int coordinateFinder(int target, vector<int>& stones) {
+        for (int idx = 0; idx < stones.size(); idx++) {
+            if (stones[idx] == target)
+                return idx;
+        }
+        return -1;
+    }
+
+    bool helper(int i, int lastJump, vector<int>& stones,
+                vector<vector<int>>& dp) {
+        if (i == stones.size() - 1)
+            return true;
+        if (dp[i][lastJump] != -1)
+            return dp[i][lastJump];
+        int curr = stones[i];
+        bool kminus1 = false, k = false, kplus1 = false;
+        if (lastJump - 1 > 0) {
+            int nextIndex = coordinateFinder(curr + lastJump - 1, stones);
+            if (nextIndex != -1)
+                kminus1 = helper(nextIndex, lastJump - 1, stones, dp);
+        }
+
+        int nextIndexK = coordinateFinder(curr + lastJump, stones);
+        if (nextIndexK != -1)
+            k = helper(nextIndexK, lastJump, stones, dp);
+
+        int nextIndexKplus1 = coordinateFinder(curr + lastJump + 1, stones);
+        if (nextIndexKplus1 != -1)
+            kplus1 = helper(nextIndexKplus1, lastJump + 1, stones, dp);
+        return dp[i][lastJump] = kminus1 || k || kplus1;
+    }
+
+public:
+    bool canCross(vector<int>& stones) {
+        if (stones[1] != 1)
+            return false;
+        int n = stones.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return helper(1, 1, stones, dp);
+    }
+};
