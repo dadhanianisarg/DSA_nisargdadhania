4482c4683671a1d3a87d609770f3f96300bcb5b0
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-18 11:25:08 +0530

    Time: 53 ms (5.91%) | Memory: 27.5 MB (53.02%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,24 +1,44 @@
 class Solution {
-    int helper(int i1, int i2, string& text1, string& text2,
-               vector<vector<int>>& dp) {
-        if (i1 == text1.size() || i2 == text2.size())
-            return 0;
-        int match = 0;
-        if (dp[i1][i2] != -1)
-            return dp[i1][i2];
-        if (text1[i1] == text2[i2]) {
-            match = 1 + helper(i1 + 1, i2 + 1, text1, text2, dp);
-        }
-        int notMatch1 = helper(i1 + 1, i2, text1, text2, dp);
-        int notMatch2 = helper(i1, i2 + 1, text1, text2, dp);
-        return dp[i1][i2] = max({match, notMatch1, notMatch2});
-    }
-
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size();
         int n2 = text2.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
-        return helper(0, 0, text1, text2, dp);
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i1 = n1 - 1; i1 >= 0; i1--) {
+            for (int i2 = n2 - 1; i2 >= 0; i2--) {
+                int match = 0;
+                if (text1[i1] == text2[i2])
+                    match = 1 + dp[i1 + 1][i2 + 1];
+                int notMatch1 = dp[i1 + 1][i2];
+                int notMatch2 = dp[i1][i2 + 1];
+                dp[i1][i2] = max({match, notMatch1, notMatch2});
+            }
+        }
+        return dp[0][0];
     }
 };
+
+// class Solution {
+//     int helper(int i1, int i2, string& text1, string& text2,
+//                vector<vector<int>>& dp) {
+//         if (i1 == text1.size() || i2 == text2.size())
+//             return 0;
+//         int match = 0;
+//         if (dp[i1][i2] != -1)
+//             return dp[i1][i2];
+//         if (text1[i1] == text2[i2]) {
+//             match = 1 + helper(i1 + 1, i2 + 1, text1, text2, dp);
+//         }
+//         int notMatch1 = helper(i1 + 1, i2, text1, text2, dp);
+//         int notMatch2 = helper(i1, i2 + 1, text1, text2, dp);
+//         return dp[i1][i2] = max({match, notMatch1, notMatch2});
+//     }
+
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n1 = text1.size();
+//         int n2 = text2.size();
+//         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+//         return helper(0, 0, text1, text2, dp);
+//     }
+// };
