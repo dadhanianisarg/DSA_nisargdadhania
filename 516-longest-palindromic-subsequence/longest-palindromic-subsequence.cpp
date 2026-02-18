9f51e02dd93964db0e018fab10df74e8555ce942
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-18 15:40:37 +0530

    Time: 216 ms (5.30%) | Memory: 75.9 MB (44.29%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++)
-            dp[i][i] = 1;
-
-        for (int len = 2; len <= n; len++) {
-            for (int l = 0; l + len - 1 < n; l++) {
-                int r = l + len - 1;
-                if (s[l] == s[r]) {
-                    if (len == 2)
-                        dp[l][r] = 2;
-                    else
-                        dp[l][r] = 2 + dp[l + 1][r - 1];
-                } else {
-                    dp[l][r] = max(dp[l + 1][r], dp[l][r - 1]);
-                }
+        string ss = s;
+        reverse(s.begin(), s.end());
+        int n1 = ss.size();
+        int n2 = s.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i1 = n1 - 1; i1 >= 0; i1--) {
+            for (int i2 = n2 - 1; i2 >= 0; i2--) {
+                int match = 0;
+                if (ss[i1] == s[i2])
+                    match = 1 + dp[i1 + 1][i2 + 1];
+                int notMatch1 = dp[i1 + 1][i2];
+                int notMatch2 = dp[i1][i2 + 1];
+                dp[i1][i2] = max({match, notMatch1, notMatch2});
             }
         }
-        return dp[0][n - 1];
+        return dp[0][0];
     }
 };
 
+
+
+// tabulation of below given memo
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+
+//         int n = s.size();
+//         vector<vector<int>> dp(n, vector<int>(n, 0));
+//         for (int i = 0; i < n; i++)
+//             dp[i][i] = 1;
+
+//         for (int len = 2; len <= n; len++) {
+//             for (int l = 0; l + len - 1 < n; l++) {
+//                 int r = l + len - 1;
+//                 if (s[l] == s[r]) {
+//                     if (len == 2)
+//                         dp[l][r] = 2;
+//                     else
+//                         dp[l][r] = 2 + dp[l + 1][r - 1];
+//                 } else {
+//                     dp[l][r] = max(dp[l + 1][r], dp[l][r - 1]);
+//                 }
+//             }
+//         }
+//         return dp[0][n - 1];
+//     }
+// };
+
+// memoization
 // class Solution {
 //     int helper(int l, int r, string& s, vector<vector<int>>& dp) {
 //         if (l < 0 || r >= s.size())
