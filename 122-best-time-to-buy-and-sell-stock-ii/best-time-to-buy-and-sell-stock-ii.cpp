afa7e97c96ec1f75b542f532017a1a83c84abd90
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-20 12:14:53 +0530

    Time: 14 ms (7.40%) | Memory: 24.7 MB (11.62%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,56 @@
 class Solution {
-    long long helper(int i, int hold, vector<int>& prices,
-                     vector<vector<long long>>& dp) {
-        if (i == prices.size())
-            return 0;
-        if (dp[i][hold] != -1)
-            return dp[i][hold];
-
-        long long profit;
-        if (hold) {
-            long long sell = prices[i] + helper(i + 1, 0, prices, dp);
-            long long hold = helper(i + 1, 1, prices, dp);
-            profit = max(sell, hold);
-        } else {
-            long long buy = -prices[i] + helper(i + 1, 1, prices, dp);
-            long long skip = helper(i + 1, 0, prices, dp);
-            profit = max(buy, skip);
-        }
-        return dp[i][hold] = profit;
-    }
-
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<long long>> dp(n, vector<long long>(2, -1));
-        return helper(0, 0, prices, dp);
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for(int hold = 0; hold < 2; hold++) {
+                long long profit;
+                if (hold) {
+                    long long sell = prices[i] + dp[i + 1][0];
+                    long long hold = dp[i + 1][1];
+                    profit = max(sell, hold);
+                } else {
+                    long long buy = -prices[i] + dp[i + 1][1];
+                    long long skip = dp[i + 1][0];
+                    profit = max(buy, skip);
+                }
+                dp[i][hold] = profit;
+            }
+        }
+        return dp[0][0];
     }
 };
+
+// class Solution {
+//     long long helper(int i, int hold, vector<int>& prices,
+//                      vector<vector<long long>>& dp) {
+//         if (i == prices.size())
+//             return 0;
+//         if (dp[i][hold] != -1)
+//             return dp[i][hold];
+
+//         long long profit;
+//         if (hold) {
+//             long long sell = prices[i] + helper(i + 1, 0, prices, dp);
+//             long long hold = helper(i + 1, 1, prices, dp);
+//             profit = max(sell, hold);
+//         } else {
+//             long long buy = -prices[i] + helper(i + 1, 1, prices, dp);
+//             long long skip = helper(i + 1, 0, prices, dp);
+//             profit = max(buy, skip);
+//         }
+//         return dp[i][hold] = profit;
+//     }
+
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<long long>> dp(n, vector<long long>(2, -1));
+//         return helper(0, 0, prices, dp);
+//     }
+// };
+
 // memory limit exceed due to storing all the states of bought instead just
 // store are we hold any stokes or not class Solution {
 //     int helper(int i, int bought, vector<int>& prices,
