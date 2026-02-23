aff057f3c7afd3519b173f61e7e0209eca0b2d68
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-23 15:44:46 +0530

    Time: 1626 ms (22.52%) | Memory: 507.4 MB (21.83%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -3,43 +3,20 @@ class Solution {
         int n1 = text1.size();
         int n2 = text2.size();
         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+
         for (int i1 = n1 - 1; i1 >= 0; i1--) {
             for (int i2 = n2 - 1; i2 >= 0; i2--) {
                 int match = 0;
                 if (text1[i1] == text2[i2])
                     match = 1 + dp[i1 + 1][i2 + 1];
+
                 int notMatch1 = dp[i1 + 1][i2];
                 int notMatch2 = dp[i1][i2 + 1];
+
                 dp[i1][i2] = max({match, notMatch1, notMatch2});
             }
         }
         return dp[0][0];
-    }
-    int helper(int prev, int curr, vector<string>& words,
-               vector<vector<int>>& dp) {
-
-        if (curr == words.size())
-            return 0;
-
-        // Shift prev by +1 to handle prev = -1
-        if (dp[prev + 1][curr] != -1)
-            return dp[prev + 1][curr];
-
-        int notTake = helper(prev, curr + 1, words, dp);
-        int take = 0;
-
-        if (prev == -1) {
-            take = 1 + helper(curr, curr + 1, words, dp);
-        } else if (words[curr].size() == words[prev].size() + 1) {
-
-            int len = longestCommonSubsequence(words[prev], words[curr]);
-
-            if (len == words[curr].size() - 1) {
-                take = 1 + helper(curr, curr + 1, words, dp);
-            }
-        }
-
-        return dp[prev + 1][curr] = max(take, notTake);
     }
 
 public:
@@ -47,7 +24,74 @@ public:
         sort(words.begin(), words.end(),
              [](string& a, string& b) { return a.size() < b.size(); });
         int n = words.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return helper(-1, 0, words, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
+                int notTake = dp[prev + 1][curr + 1];
+
+                int take = 0;
+                if (prev == -1) {
+                    take = 1 + dp[curr + 1][curr + 1];
+                } else if (words[curr].size() == words[prev].size() + 1) {
+                    int len =
+                        longestCommonSubsequence(words[prev], words[curr]);
+                    if (len == words[curr].size() - 1) {
+                        take = 1 + dp[curr + 1][curr + 1];
+                    }
+                }
+                dp[prev + 1][curr] = max(take, notTake);
+            }
+        }
+        return dp[0][0];
     }
 };
+
+// class Solution {
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n1 = text1.size();
+//         int n2 = text2.size();
+//         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+//         for (int i1 = n1 - 1; i1 >= 0; i1--) {
+//             for (int i2 = n2 - 1; i2 >= 0; i2--) {
+//                 int match = 0;
+//                 if (text1[i1] == text2[i2])
+//                     match = 1 + dp[i1 + 1][i2 + 1];
+//                 int notMatch1 = dp[i1 + 1][i2];
+//                 int notMatch2 = dp[i1][i2 + 1];
+//                 dp[i1][i2] = max({match, notMatch1, notMatch2});
+//             }
+//         }
+//         return dp[0][0];
+//     }
+//     int helper(int prev, int curr, vector<string>& words,
+//                vector<vector<int>>& dp) {
+
+//         if (curr == words.size())
+//             return 0;
+//         if (dp[prev + 1][curr] != -1)
+//             return dp[prev + 1][curr];
+
+//         int notTake = helper(prev, curr + 1, words, dp);
+
+//         int take = 0;
+//         if (prev == -1) {
+//             take = 1 + helper(curr, curr + 1, words, dp);
+//         } else if (words[curr].size() == words[prev].size() + 1) {
+//             int len = longestCommonSubsequence(words[prev], words[curr]);
+//             if (len == words[curr].size() - 1) {
+//                 take = 1 + helper(curr, curr + 1, words, dp);
+//             }
+//         }
+//         return dp[prev + 1][curr] = max(take, notTake);
+//     }
+
+// public:
+//     int longestStrChain(vector<string>& words) {
+//         sort(words.begin(), words.end(),
+//              [](string& a, string& b) { return a.size() < b.size(); });
+//         int n = words.size();
+//         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+//         return helper(-1, 0, words, dp);
+//     }
+// };
