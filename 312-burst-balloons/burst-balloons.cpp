819d387fb03fe3d1cf4827917452fb474cc139fa
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-24 16:38:33 +0530

    Time: 87 ms (92.33%) | Memory: 15.3 MB (7.68%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,28 +1,50 @@
 class Solution {
-    int helper(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
-        if (i > j)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int ans = 0;
-        for (int ind = i; ind <= j; ind++) {
-            int coins = nums[i - 1] * nums[ind] * nums[j + 1] +
-                        helper(i, ind - 1, nums, dp) +
-                        helper(ind + 1, j, nums, dp);
-            ans = max(ans, coins);
-        }
-        return dp[i][j] = ans;
-    }
-
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
-        return helper(1, n, nums, dp);
+        vector<vector<unsigned long long>> dp(n + 2, vector<unsigned long long>(n + 2, 0));
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n + 1; j++) {
+                unsigned long long ans = 0;
+                for (int ind = i; ind <= j; ind++) {
+                    unsigned long long coins = nums[i - 1] * nums[ind] * nums[j + 1] +
+                                      dp[i][ind - 1] + dp[ind + 1][j];
+                    ans = max(ans, coins);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[1][n];
     }
 };
+
+// class Solution {
+//     int helper(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+//         if (i > j)
+//             return 0;
+//         if (dp[i][j] != -1)
+//             return dp[i][j];
+//         int ans = 0;
+//         for (int ind = i; ind <= j; ind++) {
+//             int coins = nums[i - 1] * nums[ind] * nums[j + 1] +
+//                         helper(i, ind - 1, nums, dp) +
+//                         helper(ind + 1, j, nums, dp);
+//             ans = max(ans, coins);
+//         }
+//         return dp[i][j] = ans;
+//     }
+
+// public:
+//     int maxCoins(vector<int>& nums) {
+//         int n = nums.size();
+//         nums.insert(nums.begin(), 1);
+//         nums.push_back(1);
+//         vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+//         return helper(1, n, nums, dp);
+//     }
+// };
 
 // below memoization also cannot pass the testcase
 // class Solution {
