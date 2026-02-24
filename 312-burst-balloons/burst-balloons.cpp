87a2f95d0dceef8c0dec4a68e2b7d5f3faf1e5a6
Dadhania Nisarg Malaykumar <nisargdadhania8@gmail.com>
2026-02-24 16:29:52 +0530

    Time: 199 ms (44.05%) | Memory: 13.8 MB (58.36%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,91 @@
+class Solution {
+    int helper(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i > j)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int ans = 0;
+        for (int ind = i; ind <= j; ind++) {
+            int coins = nums[i - 1] * nums[ind] * nums[j + 1] +
+                        helper(i, ind - 1, nums, dp) +
+                        helper(ind + 1, j, nums, dp);
+            ans = max(ans, coins);
+        }
+        return dp[i][j] = ans;
+    }
+
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+        return helper(1, n, nums, dp);
+    }
+};
+
+// below memoization also cannot pass the testcase
+// class Solution {
+//     unordered_map<string, int> dp;
+//     string makeKey(const vector<int>& nums) {
+//         string key;
+//         for (int x : nums) {
+//             key += to_string(x) + ",";
+//         }
+//         return key;
+//     }
+
+//     int helper(vector<int> nums) {
+//         if (nums.size() == 0)
+//             return 0;
+//         string key = makeKey(nums);
+//         if (dp.count(key))
+//             return dp[key];
+//         int ans = 0;
+//         for (int j = 0; j < (int)nums.size(); j++) {
+//             int coins = nums[j];
+//             if (j >= 1)
+//                 coins *= nums[j - 1];
+//             if (j < (int)nums.size() - 1)
+//                 coins *= nums[j + 1];
+//             int erasedEle = nums[j];
+//             nums.erase(nums.begin() + j);
+//             int temp = coins + helper(nums);
+//             nums.insert(nums.begin() + j, erasedEle);
+//             ans = max(ans, temp);
+//         }
+//         return dp[key] = ans;
+//     }
+
+// public:
+//     int maxCoins(vector<int>& nums) {
+//         dp.clear();
+//         return helper(nums);
+//     }
+// };
+
+// pure brute force
+// class Solution {
+//     int helper(vector<int> nums) {
+//         if (nums.size() == 0)
+//             return 0;
+//         int ans = 0;
+//         for (int j = 0; j < (int)nums.size(); j++) {
+//             int coins = nums[j];
+//             if (j >= 1)
+//                 coins *= nums[j - 1];
+//             if (j < nums.size() - 1)
+//                 coins *= nums[j + 1];
+
+//             int erasedEle = nums[j];
+//             nums.erase(nums.begin() + j);
+//             int temp = coins + helper(nums);
+//             nums.insert(nums.begin() + j, erasedEle);
+//             ans = max(ans, temp);
+//         }
+//         return ans;
+//     }
+
+// public:
+//     int maxCoins(vector<int>& nums) { return helper(nums); }
+// };
